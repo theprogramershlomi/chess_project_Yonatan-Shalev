b8ff03c4a4def9c9d5a50e6e3b09fa4b9816ef91
shale v <shalevsarusi123@gmail.com>
2024-12-18 17:19:03 +0200

    saving

--- chess/Board.h
+++ chess/Board.h
@@ -11,7 +11,7 @@ public:
 	Board(char** board, bool isWhiteTurn);
 	Board(char* board, bool isWhiteTurn);
 	Board(std::string board, bool isWhiteTurn);
-
+	void Initialize_board();
 
 	void Move(std::string src, std::string dst);
 	short Is_Legal(std::string src, std::string dst);
--- chess/Pawn.cpp
+++ chess/Pawn.cpp
@@ -1,6 +1,6 @@
 #include "Pawn.h"
 
-bool Pawn::Is_legal_move(char** board, std::string src, std::string dst, bool isWhiteTurn)
+bool Pawn::Is_legal_move(char** board, int* src, int* dst, bool isWhiteTurn)
 {
 	return 0;
 }
