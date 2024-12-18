757ec7f171ca7723cd53d152e8962f7aa5cb016c
shale v <shalevsarusi123@gmail.com>
2024-12-18 17:29:04 +0200

    fixing git

--- chess/Board.h
+++ chess/Board.h
@@ -12,10 +12,6 @@ public:
 	Board(char* board, bool isWhiteTurn);
 	Board(std::string board, bool isWhiteTurn);
 	void Initialize_board();
-<<<<<<< HEAD
-
-=======
->>>>>>> b8ff03c4a4def9c9d5a50e6e3b09fa4b9816ef91
 
 	void Move(std::string src, std::string dst);
 	short Is_Legal(std::string src, std::string dst);
--- chess/King.cpp
+++ chess/King.cpp
@@ -1,6 +1,1 @@
 #include "King.h"
-
-bool King::Is_legal_move(char** board, int* src, int* dst, bool isWhiteTurn)
-{
-	return false;
-}
