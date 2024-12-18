52eb03e683df313136883e1b7525b865bdde8a60
shale v <shalevsarusi123@gmail.com>
2024-12-18 17:34:22 +0200

    testing

--- chess/Knight.h
+++ chess/Knight.h
@@ -4,7 +4,7 @@
 class Knight
 {
 public:
-	static short Is_legal_move(char** board, std::string src, std::string dst, bool isWhiteTurn);
+	static bool Is_legal_move(char** board, int* src, int* dst, bool isWhiteTurn);
 
 };
 
